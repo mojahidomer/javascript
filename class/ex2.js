a71b7b6e3ce78f6ca99a10ea7450007373128969
mojahidomer <38102741+mojahidomer@users.noreply.github.com>
2026-04-14 12:14:18 +0000

    new exercide

---
+++ class/ex2.js
@@ -0,0 +1,35 @@
+
+class Account {
+    #balance =0;
+    static count = 0;
+   
+    constructor(){
+        Account.count+=1;
+    }
+    addBalance(num){
+        this.#balance = this.#balance + num
+    }
+    getBalance(){
+        return this.#balance;
+    }
+}
+
+
+
+console.log(Account.count);
+
+let mojahid = new Account()
+
+let mojahid2 = new Account()
+
+let mojahid3 = new Account()
+let mojahid4 = new Account()
+let mojahid5 = new Account()
+
+mojahid5.addBalance(100)
+mojahid5.addBalance(100)
+mojahid5.addBalance(100)
+console.log(Account.count,mojahid2.getBalance(),mojahid5.getBalance());
+// consoe
+
+
